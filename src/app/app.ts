66b069115d1158aaab18281f2b4823b1f7f6bc49
Dunusigha Dewage Ravindu Nayanaka Dayarathna <raveest56@gmail.com>
2025-11-26 23:10:59 +0530

    3rd commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Poll } from './poll/poll';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Poll , RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
